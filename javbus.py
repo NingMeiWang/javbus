66e0c16b4bdbbb819602b768511b6fef1abc5e70
NingMeiWang <62290592+NingMeiWang@users.noreply.github.com>
2023-08-18 18:39:42 +0800

    Update javbus.py

--- javbus.py
+++ javbus.py
@@ -7,7 +7,9 @@ except:
     pass
 from lxml import etree
 
-cookie = os.environ.get("cookie_javbus")
+
+cookie='4fJN_2132_saltkey=ROLglA25; 4fJN_2132_lastvisit=1691911189; 4fJN_2132_sid=CXmmgr; 4fJN_2132__refer=%252Fforum%252Fhome.php%253Fmod%253Dspacecp%2526ac%253Dcredit; 4fJN_2132_sendmail=1; 4fJN_2132_seccodecSCXmmgr=13998.4618ebc78f4d2ae56a; 4fJN_2132_ulastactivity=6ab33u5QL777jG2Ee0y8kJS%2FdqrnAHs4zZNpcZhdrNArh1QSdqjn; 4fJN_2132_lastcheckfeed=412165%7C1691914820; 4fJN_2132_checkfollow=1; 4fJN_2132_lip=162.158.171.27%2C1691890076; 4fJN_2132_auth=505dPmWHcaaqEvr1bL%2FUWypCQFwgLl50HazBoNfgfwFmMfv6sA%2F0%2BxEDk4dVSKUVq3ygCjLzGVfMsBWkr30RUAh3qDE; bus_auth=214dKXG1s40muOq9s2nDsb7JHXQeVrtJk%2BgECXKdlciNtr%2F0xUKDfo1YNGanXXh5; 4fJN_2132_nofavfid=1; 4fJN_2132_onlineusernum=9588; 4fJN_2132_checkpm=1; 4fJN_2132_lastact=1691914831%09home.php%09spacecp'
+
 
 def run(*arg):
     msg = ""
